38f585a01f6d74d28be52bb6e806113d2673cf25
yincheng <302869565@qq.com>
2020-12-17 20:05:06 +0800

    test

---
+++ git-test/src/main/java/com/hs/yin/TestGitCommit.java
@@ -0,0 +1,12 @@
+package com.hs.yin;
+
+/**
+ * @Author: yc
+ * @Description:
+ * @Date: Create in 20:03 2020/12/17
+ */
+public class TestGitCommit {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
